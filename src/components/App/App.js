8a3fd54aeb2d466be26c3afbadfa5180e923b930
Mykola Voronin <nvoronin@rubygarage.org>
2019-07-10 18:48:30 +0300

    Fix snapshot tests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+
 import Home from '../Home';
+
 import 'antd/dist/antd.css';
 import '../../assets/styles/app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Home />
+  </div>
+);
 
 export default App;
