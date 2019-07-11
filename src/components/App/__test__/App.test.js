dea96f8f4d0b77c8ef25309ff984a3760c5302e7
Mykola Voronin <nvoronin@rubygarage.org>
2019-07-11 08:40:16 +0300

    Fix tests (#12)
    * Fix snapshot tests
    
    * Add example of container test

--- src/components/App/__test__/App.test.js
+++ src/components/App/__test__/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
+
 import App from '../App';
 
 describe('Component: App', () => {
   const AppComponent = shallow(<App />);
+
   it('should match its snapshot', () => {
-    expect(shallowToJson(AppComponent)).toMatchSnapshot();
+    expect(AppComponent).toMatchSnapshot();
   });
 });
